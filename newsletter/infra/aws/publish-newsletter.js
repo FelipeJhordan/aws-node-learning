e27ae61b02d876342142142beb84936c0f52f80a
felipejhordan <felipejordan.alves@gmail.com>
2022-05-12 18:06:14 -0300

    feat(publish): create the publisher method

---
+++ newsletter/infra/aws/publish-newsletter.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const AWS = require("aws-sdk")
+
+module.exports.handler = async (event) => {
+    try {
+        let config = {
+            region: process.env.AWS_REGION
+        }
+
+
+        if(process.env.IS_OFFLINE) {
+            config.endpoint = process.env.SNS_ENDPOINT_LOCAL
+        }
+
+        const sns = new AWS.SNS(config)
+
+        const { subject, introduction, recipes } = JSON.parse(event.body)
+
+        const params = {
+            Message: JSON.stringify({subject, introduction, recipes}),
+            TopicArn: process.env.SNS_ARN
+        }
+
+        await sns.publish(params).promise()
+
+        console.info("PUBLISH_NEWSLETTER")
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({})
+        }
+    } catch(e) {
+        console.error(e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: error.Message})
+        }
+    }
+}
