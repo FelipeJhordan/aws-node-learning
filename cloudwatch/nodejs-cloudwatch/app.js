b8914097738a0217b2c8b7082440ee4dcf37069a
FelipeJhordanSecondary <felipejhordan.alves@gmail.com>
2022-05-27 18:06:33 -0300

    feat: import dotenv and create a route to handle error

--- cloudwatch/nodejs-cloudwatch/app.js
+++ cloudwatch/nodejs-cloudwatch/app.js
@@ -1,11 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+require('dotenv-safe').config()
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const {  middleware  } = require('./middlewares/error-handler');
 
 var app = express();
 
@@ -19,7 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
+app.use('/error', (req, res, next) => {
+  throw new Error('Just a test error')
+})
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -27,15 +34,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(middleware)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
