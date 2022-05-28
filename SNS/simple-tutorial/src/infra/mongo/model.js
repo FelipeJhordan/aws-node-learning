f9c21339673fc1a9b9cf49ab04d008104da7ae54
FelipeJhordanSecondary <felipejhordan.alves@gmail.com>
2022-05-27 22:29:10 -0300

    feat: creat simple sns project to study about sns

---
+++ SNS/simple-tutorial/src/infra/mongo/model.js
@@ -0,0 +1,22 @@
+export default (conn, mongoose) => {
+    return conn.model("notifications", new mongoose.Schema({
+      to: {
+        type: String,
+        required: true,
+      },
+      body: {
+        type: String,
+        required: true,
+      },
+      sended: {
+        type: Boolean,
+        default: false,
+        required: true,
+      },
+      createdAt: { 
+        type: Date, 
+        default: Date.now, 
+      },
+      sendedAt: Date,
+    }));
+  };
