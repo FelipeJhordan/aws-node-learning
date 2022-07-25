5ae00f7c9e59864435529b004f6549be4038628a
felipejhordan <felipejordan.alves@gmail.com>
2022-07-24 23:59:01 -0300

    feat(serverless): create a handler file with methods that attempts to http request for crud events POST and GET

--- serverless-estudo/serverless-with-dynamo-db/products-api/src/handlers.ts
+++ serverless-estudo/serverless-with-dynamo-db/products-api/src/handlers.ts
@@ -1,15 +1,49 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_NAME = "ProductsTable";
+
+export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const reqBody = JSON.parse(event.body as string);
+  const product = {
+    ...reqBody,
+    productID: uuidv4(),
+  };
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: product,
+    })
+    .promise();
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+    body: JSON.stringify(product),
+  };
+};
+
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id;
+
+  const output = await docClient
+    .get({
+      TableName: TABLE_NAME,
+      Key: {
+        productID: id,
       },
-      null,
-      2,
-    ),
+    })
+    .promise();
+
+  if (!output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "not found" }),
+    };
+  }
+  return {
+    statusCode: 2000,
+    body: JSON.stringify(output.Item),
   };
 };
